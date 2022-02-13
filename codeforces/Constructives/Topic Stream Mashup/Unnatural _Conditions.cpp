a27ea29932f321218033ba7fd9e40397d40856ca
abir10t <aabir.10t@gmail.com>
2022-02-14 01:43:42 +0600

    solved B

---
+++ codeforces/Constructives/Topic Stream Mashup/Unnatural _Conditions.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+int n = 1000;
+string s = string(n, '4');
+string t = string(n, '5');
+
+
+cout<<s<<5<<endl<<t<<endl;
+
+return 0;
+
+
+}
